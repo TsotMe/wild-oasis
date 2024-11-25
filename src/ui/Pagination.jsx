682e0a9acc9413387c99c134bbf7c093f2af465a
tsotne <tsotnemeskhuri@gmail.com>
2024-11-25 18:54:00 +0400

    Use PAGE_SIZE constant inside Pagination component

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 // import Butons from "./Buttons";
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../utils/constants";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -60,8 +61,6 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
-
-const PAGE_SIZE = 10;
 
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
