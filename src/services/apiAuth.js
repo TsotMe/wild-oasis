9e569a22b2f193077b14e8ce5486d0fd4b8687f9
tsotne <tsotnemeskhuri@gmail.com>
2024-11-26 15:22:22 +0400

    Add user logout feature

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -26,3 +26,11 @@ export async function getCurrentUser() {
 
   return user || null;
 }
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+}
