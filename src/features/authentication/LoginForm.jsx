dd9be97ef042b0e29144d3295cc70e6364cfa8a2
tsotne <tsotnemeskhuri@gmail.com>
2025-01-08 16:36:58 +0400

    Pre fill test user credentials

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("user@example.com");
+  const [password, setPassword] = useState("user123");
 
   const { login, isLoading } = useLogin();
 
